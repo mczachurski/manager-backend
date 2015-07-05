b5637d35d1258ac706129f729772c7e25cc829a3
Marcin Czachurski <mczachurski@icloud.com>
2015-07-05 11:03:25 +0200

    Fix database store in WebApi.

--- SunLine.Manager.WebApi/Startup.cs
+++ SunLine.Manager.WebApi/Startup.cs
@@ -35,6 +35,7 @@ namespace SunLine.Manager.WebApi
         public void ConfigureServices(IServiceCollection services)
         {            
             services.AddEntityFramework()
+                .AddInMemoryStore()
                 .AddDbContext<DatabaseContext>();
             
             services.AddMvc();
